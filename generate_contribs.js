d76a15add6c3e9fcaba5ba1cfceab64134c6713b
Tom Hockett <tom.hockett@gmail.com>
2018-07-22 17:39:03 -0500

    markup

--- generate_contribs.js
+++ generate_contribs.js
@@ -6,8 +6,8 @@ const { exec } = require('child_process')
 const execAsync = promisify(exec)
 const writeFileAsync = promisify(writeFile)
 
-const AUTHOR = 'username'
-const EMAIL = 'email'
+const AUTHOR = 'tomhockett'
+const EMAIL = 'tom.hockett@gmail.com'
 const DAYS_TO_GENERATE = 365
 
 const dateMinusGivenDays = days => {
